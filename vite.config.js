34bc90bbf45fe996c5d31d53fe80b2829f41958c
Szabolcs Hovanecz <hovszabolcs@gmail.com>
2024-10-11 02:13:33 +0200

    package-lock

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react'
+
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: '0.0.0.0'
+        }
+    },
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        react()
     ],
-});
+})
+
+
