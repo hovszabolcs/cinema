f65178a7263da7d9b4fef1443f1273e1ca21a8a6
Szabolcs Hovanecz <hovszabolcs@gmail.com>
2024-10-13 14:27:59 +0200

    Seeders

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,25 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    /**
+     * @property string $id
+     * @property string $name
+     * @property string $email
+     * @property string $password
+     * @property string $remember_token
+     * @property string $email_verified_at
+     * @property string $created_at
+     * @property string $updated_at
+     */
+
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Movie;
+use App\Models\Screening;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +17,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make('password')
         ]);
+
+        Schema::disableForeignKeyConstraints();
+
+        DB::table('screenings')->truncate();
+
+        DB::table('movies')->truncate();
+
+        Movie::factory(10)->create()->each(function ($movie) {
+            Screening::factory(rand(3, 5))->create(['movie_id' => $movie->id]);
+        });
+
+        Schema::enableForeignKeyConstraints();
     }
 }
