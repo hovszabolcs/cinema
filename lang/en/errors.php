9d991d595ceca1e0f293f7094c9dc99cfb9d62e1
Szabolcs Hovanecz <hovszabolcs@gmail.com>
2024-10-13 20:06:49 +0200

    Non-translated language pack

---
+++ lang/en/errors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'bad_request'   => 'Bad Request',
+    'unauthorized'  => 'Unauthorized',
+    'forbidden'     => 'Forbidden',
+    'not_found'     => 'Not Found',
+    'unknown_error' => 'Unknown error'
+];
---
+++ lang/hu/errors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'bad_request'   => 'Hibás kérés',
+    'unauthorized'  => 'Nem megfelelő jogosultság',
+    'forbidden'     => 'Elutasítva',
+    'not_found'     => 'Nem található',
+    'unknown_error' => 'Ismeretlen hiba'
+];
